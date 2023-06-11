2c9f48aaf3690abea1bbf01402449f86e3cf58c2
Learza7 <remystryx@gmail.com>
2023-06-11 22:46:09 +0200

    add responsive not really ok + loader

--- src/components/RankingTable.jsx
+++ src/components/RankingTable.jsx
@@ -12,12 +12,16 @@ function RankingTable() {
   const [modalOpen, setModalOpen] = useState(false);
   const [currentPlayer, setCurrentPlayer] = useState(null);
 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    
+
     fetch("https://cate-rankings-backend.herokuapp.com/players")
       .then((response) => response.json())
-      .then((data) => setPlayers(data));
+      .then((data) => {
+        setPlayers(data);
+        setLoading(false);
+      });
   }, []);
 
   const sortData = (field) => {
@@ -45,7 +49,7 @@ function RankingTable() {
 
   const openModal = (player) => {
     console.log(player);  // logs the player when the modal opens
-  
+
     fetch(`https://cate-rankings-backend.herokuapp.com/players/${player.fideId}/tournaments`)
       .then((response) => response.json())
       .then((data) => {
@@ -54,10 +58,10 @@ function RankingTable() {
         console.log(playerWithTournaments);  // logs the player with the tournaments data
         setCurrentPlayer(playerWithTournaments);
       });
-  
+
     setModalOpen(true);
   };
-  
+
   const closeModal = () => {
     setModalOpen(false);
     setCurrentPlayer(null);
@@ -73,83 +77,98 @@ function RankingTable() {
       transform: 'translate(-50%, -50%)',
       backgroundColor: 'white',
       borderRadius: '0.5rem',
-      padding: '0rem'
+      padding: '1rem',
+      width: '100%',
+      maxWidth: '1000px',
     },
     overlay: {
       backgroundColor: 'rgba(0, 0, 0, 0.75)'
     }
   };
-  
+
 
   return (
     <>
-      <div className="overflow-auto shadow-lg rounded-lg">
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-200 sticky top-0">
-            <tr>
-              <th scope="col" className="px-6 py-3 text-left text-m font-semibold text-gray-600 uppercase tracking-wider cursor-pointer" onClick={() => sortData('lastName')}>
-                Name<span className="inline-block ml-2">{sortConfig && sortConfig.field === 'lastName' ? (sortConfig.direction === 'ascending' ? '↑' : '↓') : '  '}</span>
-              </th>
-              <th scope="col" className="px-6 py-3 text-left text-m font-semibold text-gray-600 uppercase tracking-wider cursor-pointer" onClick={() => sortData('classical')}>
-                Classical<span className="inline-block ml-2">{sortConfig && sortConfig.field === 'classical' ? (sortConfig.direction === 'ascending' ? '↑' : '↓') : '  '}</span>
-              </th>
-              <th scope="col" className="px-6 py-3 text-left text-m font-semibold text-gray-600 uppercase tracking-wider cursor-pointer" onClick={() => sortData('rapid')}>
-                Rapid<span className="inline-block ml-2">{sortConfig && sortConfig.field === 'rapid' ? (sortConfig.direction === 'ascending' ? '↑' : '↓') : '  '}</span>
-              </th>
-              <th scope="col" className="px-6 py-3 text-left text-m font-semibold text-gray-600 uppercase tracking-wider cursor-pointer" onClick={() => sortData('blitz')}>
-                Blitz<span className="inline-block ml-2">{sortConfig && sortConfig.field === 'blitz' ? (sortConfig.direction === 'ascending' ? '↑' : '↓') : '  '}</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {sortedPlayers.map((player, index) => (
-              <tr key={player.fideId} className={`${index % 2 === 0 ? 'bg-gray-50' : 'bg-white'} hover:bg-gray-100 transition-colors duration-200`}>
-                <td className="px-6 py-4 whitespace-nowrap text-gray-800">{`${player.lastName} ${player.firstName}`}</td>
-                <td className={`px-6 py-4 whitespace-nowrap text-gray-800 ${player.classicalVariation !== 0 ? "cursor-pointer" : ""}`} onClick={() => player.classicalVariation !== 0 && openModal(player)}>
-                  {`${player.classical != null ? player.classical : ""}`}
-                  <span className={`${player.classicalVariation > 0 ? "text-green-500" : "text-red-500"} text-base`}>
-                    {player.classicalVariation !== 0 ? ' (' + (player.classicalVariation > 0 ? '↑' : '↓') + Math.abs(player.classicalVariation) + ')' : ''}
-                  </span>
-                </td>
-                <td className={`px-6 py-4 whitespace-nowrap text-gray-800 ${player.rapidVariation !== 0 ? "cursor-pointer" : ""}`} onClick={() => player.rapidVariation !== 0 && openModal(player)}> 
-                  {`${player.rapid != null ? player.rapid : ""}`}
-                  <span className={`${player.rapidVariation > 0 ? "text-green-500" : "text-red-500"} text-base`}>
-                    {player.rapidVariation !== 0 ? ' (' + (player.rapidVariation > 0 ? '↑' : '↓') + Math.abs(player.rapidVariation) + ')' : ''}
-                  </span>
-                </td>
-                <td className={`px-6 py-4 whitespace-nowrap text-gray-800 ${player.blitzVariation !== 0 ? "cursor-pointer" : ""}`} onClick={() => player.blitzVariation !== 0 && openModal(player)}>
-                  {`${player.blitz != null ? player.blitz : ""}`}
-                  <span className={`${player.blitzVariation > 0 ? "text-green-500" : "text-red-500"} text-base`}>
-                    {player.blitzVariation !== 0 ? ' (' + (player.blitzVariation > 0 ? '↑' : '↓') + Math.abs(player.blitzVariation) + ')' : ''}
-                  </span>
-                </td>
 
-              </tr>
-            ))}
 
-          </tbody>
-          <Modal
-  isOpen={modalOpen}
-  onRequestClose={closeModal}
-  contentLabel="Player Details"
-  style={customStyles}
->
-  {currentPlayer &&
-    <div className="relative">
-      {/* <h2>{`${currentPlayer.lastName} ${currentPlayer.firstName}`}</h2> */}
-      <TournamentList tournaments={currentPlayer.tournaments} />
-      {/* <button 
+      {
+        loading ? (
+          <div className="flex justify-center items-center h-96">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900 shadow-md bg-white bg-opacity-20"></div>
+      </div>
+        ) : (
+          <div className="overflow-x-scroll md:overflow-x-auto rounded-lg">
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-200 sticky top-0">
+                <tr>
+                <th scope="col" className="px-2 md:px-6 py-3 text-left md:text-m font-semibold text-gray-600 uppercase tracking-wider cursor-pointer" onClick={() => sortData('lastName')}>
+                  Name<span className="inline-block ml-2">{sortConfig && sortConfig.field === 'lastName' ? (sortConfig.direction === 'ascending' ? '↑' : '↓') : '  '}</span>
+                </th>
+                  <th scope="col" className="px-2 md:px-6 py-3 text-left md:text-m font-semibold text-gray-600 uppercase tracking-wider cursor-pointer" onClick={() => sortData('classical')}>
+                    Classical<span className="inline-block ml-2">{sortConfig && sortConfig.field === 'classical' ? (sortConfig.direction === 'ascending' ? '↑' : '↓') : '  '}</span>
+                  </th>
+                  <th scope="col" className="px-2 md:px-6 py-3 text-left md:text-m font-semibold text-gray-600 uppercase tracking-wider cursor-pointer" onClick={() => sortData('rapid')}>
+                    Rapid<span className="inline-block ml-2">{sortConfig && sortConfig.field === 'rapid' ? (sortConfig.direction === 'ascending' ? '↑' : '↓') : '  '}</span>
+                  </th>
+                  <th scope="col" className="px-2 md:px-6 py-3 text-left md:text-m font-semibold text-gray-600 uppercase tracking-wider cursor-pointer" onClick={() => sortData('blitz')}>
+                    Blitz<span className="inline-block ml-2">{sortConfig && sortConfig.field === 'blitz' ? (sortConfig.direction === 'ascending' ? '↑' : '↓') : '  '}</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {sortedPlayers.map((player, index) => (
+                  <tr key={player.fideId} className={`${index % 2 === 0 ? 'bg-gray-50' : 'bg-white'} hover:bg-gray-100 transition-colors duration-200`}>
+                    <td className="px-6 py-4 whitespace-nowrap text-gray-800">{`${player.lastName} ${player.firstName}`}</td>
+                    <td className={`px-6 py-4 whitespace-nowrap text-gray-800 ${player.classicalVariation !== 0 ? "cursor-pointer" : ""}`} onClick={() => player.classicalVariation !== 0 && openModal(player)}>
+                      {`${player.classical != null ? player.classical : ""}`}
+                      <span className={`${player.classicalVariation > 0 ? "text-green-500" : "text-red-500"} text-base`}>
+                        {player.classicalVariation !== 0 ? ' (' + (player.classicalVariation > 0 ? '↑' : '↓') + Math.abs(player.classicalVariation) + ')' : ''}
+                      </span>
+                    </td>
+                    <td className={`px-6 py-4 whitespace-nowrap text-gray-800 ${player.rapidVariation !== 0 ? "cursor-pointer" : ""}`} onClick={() => player.rapidVariation !== 0 && openModal(player)}>
+                      {`${player.rapid != null ? player.rapid : ""}`}
+                      <span className={`${player.rapidVariation > 0 ? "text-green-500" : "text-red-500"} text-base`}>
+                        {player.rapidVariation !== 0 ? ' (' + (player.rapidVariation > 0 ? '↑' : '↓') + Math.abs(player.rapidVariation) + ')' : ''}
+                      </span>
+                    </td>
+                    <td className={`px-6 py-4 whitespace-nowrap text-gray-800 ${player.blitzVariation !== 0 ? "cursor-pointer" : ""}`} onClick={() => player.blitzVariation !== 0 && openModal(player)}>
+                      {`${player.blitz != null ? player.blitz : ""}`}
+                      <span className={`${player.blitzVariation > 0 ? "text-green-500" : "text-red-500"} text-base`}>
+                        {player.blitzVariation !== 0 ? ' (' + (player.blitzVariation > 0 ? '↑' : '↓') + Math.abs(player.blitzVariation) + ')' : ''}
+                      </span>
+                    </td>
+
+                  </tr>
+                ))}
+
+              </tbody>
+
+
+            </table>
+          </div>)
+      }
+
+
+
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Player Details"
+        style={customStyles}
+      >
+        {currentPlayer &&
+          <div className="relative">
+            {/* <h2>{`${currentPlayer.lastName} ${currentPlayer.firstName}`}</h2> */}
+            <TournamentList tournaments={currentPlayer.tournaments} />
+            {/* <button 
         onClick={closeModal} 
         className="absolute top-1 right-1 bg-blue-500 text-white font-bold py-2 px-4 rounded-full hover:bg-blue-700 transition duration-200"
       >
         X
       </button> */}
-    </div>
-  }
-</Modal>
-
-        </table>
-      </div>
+          </div>
+        }
+      </Modal>
     </>
   );
 }
